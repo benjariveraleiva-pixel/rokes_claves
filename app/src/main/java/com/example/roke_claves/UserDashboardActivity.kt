c224eadf93ed4738c713febb7cbcc37942457c6f
benja <benjariveraleiva@gmail.com>
2025-11-30 19:02:33 -0300

    Se modifico las la llave de acceso de usuario, el dashboard para usuario y se añadio un activity mas de evento de acceso

--- app/src/main/java/com/example/roke_claves/UserDashboardActivity.kt
+++ app/src/main/java/com/example/roke_claves/UserDashboardActivity.kt
@@ -1,20 +1,43 @@
 package com.example.roke_claves
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class UserDashboardActivity : AppCompatActivity() {
+
+    private lateinit var btnEventos: Button
+    private lateinit var btnAcceso: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_user_dashboard)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Enlazar botones con IDs reales
+        btnEventos = findViewById(R.id.btn_leventos)
+        btnAcceso = findViewById(R.id.btn_pacceso)
+
+        // Ir a Lista de Eventos
+        btnEventos.setOnClickListener {
+            val intent = Intent(this, user_evento_acceso::class.java)
+            startActivity(intent)
+        }
+
+        // Ir a Llave de Acceso
+        btnAcceso.setOnClickListener {
+            val intent = Intent(this, llave_acceso::class.java)
+            startActivity(intent)
         }
     }
 }
